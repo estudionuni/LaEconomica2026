3ad3463edccae11f4724b3c53439c618c1b40a66
estudionuni <estudionuni@gmail.com>
2026-01-02 15:13:09 -0300

    Refactor CategoriesList component to simplify mapping and improve readability

--- client/src/components/CategoriesList.jsx
+++ client/src/components/CategoriesList.jsx
@@ -6,19 +6,18 @@ const CategoriesList = () => {
   return (
     <>
       <section className="flex w-full sm:w-1/2 h-fit gap-2">
-        {categoriasList.map((categoria, imagen, path) => {
-          const Icon = imagen;
+        {categoriasList.map((item) => {
+          const Icon = item.imagen;
+
           return (
-            <>
-              <Link
-                to={`/categoria/${path}`}
-                key={path}
-                className="flex flex-col size-fit"
-              >
-                <Icon classname="size-5 bg-red-500 text-white p-2 mx-auto" />
-                <p className="w-fit mx-auto">{categoria}</p>
-              </Link>
-            </>
+            <Link
+              key={item.path}
+              to={`/categoria/${item.path}`}
+              className="flex flex-col size-fit"
+            >
+              <Icon className="size-5 bg-red-500 text-white p-2 mx-auto" />
+              <p className="w-fit mx-auto">{item.categoria}</p>
+            </Link>
           );
         })}
       </section>
