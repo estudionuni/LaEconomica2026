6ae9706217194007b84c1c37d5e1a32a67f2a36e
estudionuni <estudionuni@gmail.com>
2026-01-02 15:17:06 -0300

    Enhance styling of category links in CategoriesList component

--- client/src/components/CategoriesList.jsx
+++ client/src/components/CategoriesList.jsx
@@ -13,7 +13,7 @@ const CategoriesList = () => {
             <Link
               key={item.path}
               to={`/categoria/${item.path}`}
-              className="flex flex-col size-fit"
+              className="flex flex-col size-fit p-2 rounded-md border border-neutral-500"
             >
               <Icon className="size-10 bg-red-500 text-white p-2 mx-auto" />
               <p className="w-fit mx-auto text-center">{item.categoria}</p>
