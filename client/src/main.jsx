c8175a50829e614631851783c54b6a8962dd5985
Nuni Estudio <estudionuni@gmail.com>
2026-01-02 02:38:47 -0300

    Switch from BrowserRouter to HashRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { AppContextProvider } from "./context/AppContext";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/LaEconomica2026">
+  <HashRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
