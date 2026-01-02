b701e06bbf81cbd1f5dedbf9455ec02e1e2a3edf
estudionuni <estudionuni@gmail.com>
2026-01-02 15:20:24 -0300

    Add overflow-x-auto to CategoriesList section for horizontal scrolling

--- client/src/components/CategoriesList.jsx
+++ client/src/components/CategoriesList.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const CategoriesList = () => {
   return (
     <>
-      <section className="flex w-full sm:w-1/2 h-fit gap-2">
+      <section className="flex w-full sm:w-1/2 h-fit gap-2 overflow-x-auto">
         {categoriasList.map((item) => {
           const Icon = item.imagen;
 
@@ -15,7 +15,7 @@ const CategoriesList = () => {
               to={`/categoria/${item.path}`}
               className="flex flex-col size-fit p-2 rounded-md border border-neutral-500"
             >
-              <Icon className="size-10 bg-red-500 text-white p-2 mx-auto" />
+              <Icon className="size-10 bg-red-500 text-white p-2 mx-auto rounded-full" />
               <p className="w-fit mx-auto text-center">{item.categoria}</p>
             </Link>
           );
